a294c11d3b8f2fa63e0d214407bb24e9cacc2f65
dianapronckute <diana.pronckute@va.lv>
2021-11-11 20:16:03 +0200

    pie chart added

--- screens/VisualiseBudget.js
+++ screens/VisualiseBudget.js
@@ -1,14 +1,47 @@
 import React from 'react';
-import {View, Button, Text} from 'react-native'
+import {View, Button, Text, Image, StyleSheet, FlatList} from 'react-native'
 
 const VisualiseBudget = () => {
 
     return (
         <View style={{ flex:1, justifyContent: 'center', alignContent: 'center'}}>
-        <Text style ={{ fontSize: 30}}>Tut budet kruglaja kartinka</Text>
+        <Text style ={{ fontSize: 30}}>Visualise Your budget</Text>
+        <Image
+        style={styles.Logo}
+        source={require('../screens/pie-chart.png')}
+              />
+        <FlatList
+        data={[
+          {key : 'Flat'},
+          {key: 'Health'},
+          {key: 'Food'},
+          {key: 'Pet'},
+          {key: 'Hobby'},
+          
+        ]}
+        renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+      />
         </View>
       
       );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 50,
+  },
+  
+  logo: {
+    width: 66,
+    height: 58,
+  },
+   item: {
+     padding: 10,
+     fontSize: 12,
+     height: 40,
+     marginVertical: 18,
+    marginHorizontal: 16,
+   },
+});
 
 export default VisualiseBudget
